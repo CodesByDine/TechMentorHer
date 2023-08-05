c2f35c176069d3f2b4698c607cc21f1bcdbe8bf2
Geraldine <c4dn5wld3@vossie.net>
2023-08-05 23:05:47 +0200

    Login

--- Backend/home.php
+++ Backend/home.php
@@ -17,32 +17,32 @@
     <title>TechMentorHer</title>
   </head>
   <body>
-    <p>This is the homepage</p>
-    <?php
 
-    $id = $_SESSION['id'];
-    $query = mysqli_query($con, "SELECT*FROM users WHERE id=$id");
+   <?php
+
+    $username = $_SESSION['idnumber'];
+    $query = mysqli_query($con, "SELECT*FROM users WHERE idnumber=$idnumber");
 
     while($result = mysqli_fetch_assoc($query)){
-      $res_username = $result['username'];
+      $res_uname = $result['username'];
       $res_email = $result['email'];
       $res_location = $result['location'];
-      $res_id = $result['id'];
-
+      $res_IdNumber = $result['idnumber'];
     }
 
-    echo "<a href='edit.php?id=$res_id'>Change Profile</a>";
+    echo "<a href='edit.php?username=$res_username'>Change Profile</a>";
     ?>
+    <p>This is the homepage</p>
     <a href="logout.php"><button class="btn">Log out</button></a>
 
     <div class="main-box top">
       <div class="top">
         <div class="box">
-          <p>Hello <b>Dani</b>👋🏽, Welcome to TechMentorHer!🎉</p>
+          <p>Hello <b><?php echo $res_Uname; ?></b>👋🏽, Welcome to TechMentorHer!🎉</p>
         </div>
 
         <div class="box">
-          <p>Your location is <b>Riversands</b>, Welcome to TechMentorHer!🎉</p>
+          <p>Your location is <b><?php echo $res_Location; ?></b>, Welcome to TechMentorHer!🎉</p>
         </div>
       </div>
     </div>
